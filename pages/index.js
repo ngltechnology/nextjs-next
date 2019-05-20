62f7e846d12f1fb25ba3294d94b724a981c0cdc1
Takanori Fukuyama <takanori.fukuyama@ngl.technology>
2019-05-21 04:55:52 +0900

    initialize

---
+++ pages/index.js
@@ -0,0 +1,129 @@
+import React,{ Fragment, Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import MuiLink from '@material-ui/core/Link';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
+import { withStyles, Tabs, Tab } from "@material-ui/core"
+
+function MadeWithLove() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Built with love by the '}
+      <MuiLink color="inherit" href="https://material-ui.com/">
+        Material-UI
+      </MuiLink>
+      {' team.'}
+    </Typography>
+  );
+}
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  logo: {
+    marginLeft: "20px",
+    marginRight: "20px"
+  },
+  tabsRoot: {
+    display: "flex",
+    borderBottom: '1px solid #e8e8e8',
+  },
+  tabsIndicator: {
+    backgroundColor: '#1890ff',
+  },
+  tabRoot: {
+    textTransform: 'initial',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing.unit * 4,
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      color: '#40a9ff',
+      opacity: 1,
+    },
+    '&$tabSelected': {
+      color: '#1890ff',
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    '&:focus': {
+      color: '#40a9ff',
+    },
+  },
+  tabSelected: {},
+  typography: {
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class Index extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <Fragment>
+        <div className={classes.root}>
+          <h3 className={classes.logo}>FREEWORKS</h3>
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            classes={{ root: classes.tabsRoot, indicator: classes.tabsIndicator }}
+          >
+            <h3>FREEWORKS</h3>
+            <Tab
+              disableRipple
+              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+              label="Servise"
+            />
+            <Tab
+              disableRipple
+              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+              label="Company"
+            />
+            <Tab
+              disableRipple
+              classes={{ root: classes.tabRoot, selected: classes.tabSelected }}
+              label="Design"
+            />
+          </Tabs>
+        </div>
+        <Container maxWidth="sm">
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Next.js v4-beta example
+            </Typography>
+            <Link href="/about" color="secondary">
+              Go to the about page
+            </Link>
+            <ProTip />
+            <MadeWithLove />
+          </Box>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+export default withStyles(styles)(Index);
